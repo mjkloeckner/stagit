f674c660522ee44a19660240d3183d5ca5acd326
mjkloeckner <martin.cachari@gmail.com>
2023-01-06 00:17:18 -0300

    add id selector to header table

--- stagit.c
+++ stagit.c
@@ -540,7 +540,7 @@ writeheader(FILE *fp, const char *title)
 	xmlencode(fp, name, strlen(name));
 	fprintf(fp, " Atom Feed (tags)\" href=\"%stags.xml\" />\n", relpath);
 	fprintf(fp, "<link rel=\"stylesheet\" type=\"text/css\" href=\"%sstyle.css\" />\n", relpath);
-	fputs("</head>\n<body>\n<table><tr><td>", fp);
+	fputs("</head>\n<body>\n<table id=\"header-table\"><tr><td>", fp);
 	fprintf(fp, "<a href=\"https://git.kloeckner.com.ar\"><img src=\"%slogo.png\" alt=\"\" width=\"64\" height=\"64\" /></a>",
 	        relpath, relpath);
 	fputs("</td><td><h1 id=\"repo-name\">", fp);
